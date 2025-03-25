70fdbe81e595246d8566b8129d1c087bcb598148
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2025-03-25 19:04:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,39 @@
-import cv2, time
-from ultralytics import YOLO
-import queue
-from threading import Thread
+import queue, threading, time
 
-def thread_safe_predict(frame, buffer):
-    print('predict')
-    results = model.predict(frame, stream=True, verbose=False)
-    print('put')
-    buffer.put(results)
+def worker(i):
+    for i in range(10):
+        time.sleep(1)
+    return f'{i} finished'
 
-video_path = "/content/reverse.mp4"
-cap = cv2.VideoCapture(video_path)
-buffer = queue.Queue()
-i = 0
-t = time.time()
-while cap.isOpened() and i <= 32:
-    _, frame = cap.read()
-    Thread(target=thread_safe_predict, args=(frame, buffer)).start()
-    if not buffer.empty():
-      results = buffer.get()
-      #results[0].show()
-      i += 1
-print(t - time.time())
+class YOLOThreadSafe(threading.Thread):
+    def __init__(self, identifier=0):
+        super().__init__()
+        self.identifier = identifier
+        self.running = False
+        self.engine = worker
+        
+    def run(self):
+        while True:
+            if inputs.qsize() > 0 and not self.running:
+                print(f'Processor {self.identifier}, Get a new task')
+                self.running = True
+                ret = self.engine(self.identifier)
+                outputs.put(ret)
+                print(f'Processor {self.identifier}, Finished a task')
+                self.running = False
+
+inputs = queue.Queue()
+outputs = queue.Queue()
+
+workers = {}
+max_workers = 5
+for i in range(max_workers):
+    workers[i] = YOLOThreadSafe(i)
+    workers[i].start()
+
+for i in range(100):
+    inputs.put(i)
+    if outputs.qsize() > 0:
+        print('main output:', outputs.get())
+    time.sleep(1)
+    
