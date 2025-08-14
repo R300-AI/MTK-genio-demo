5a26b2b1f84753eb8369f608af29dcfdb688254c
MarkovChenITRI <140595764+MarkovChenITRI@users.noreply.github.com>
2025-08-14 20:59:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,44 +3,46 @@ import cv2, argparse
 import asyncio
 
 parser = argparse.ArgumentParser(description="YOLO Async Inference with asyncio")
+
 parser.add_argument("--video_path", type=str, default="./data/video.mp4")
 parser.add_argument("--tflite_model", type=str, default="./models/yolov8n_float32.tflite")
+parser.add_argument("--num_workers", type=int, default=2, help="Number of inference workers")
 args = parser.parse_args()
 
 cap = cv2.VideoCapture(args.video_path)
 model = YOLO(args.tflite_model, task="detect")
 
 frame_queue = asyncio.Queue(maxsize=100)
-result_queue = asyncio.Queue(maxsize=100)
 
 async def preprocess():
     print("[讀取器] 啟動")
-    loop = asyncio.get_running_loop()
+    index = 0
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-        await frame_queue.put(frame)
+        await frame_queue.put((index, frame))
+        index += 1
+    # 放入 None 給每個 inference worker
+    for _ in range(args.num_workers):
+        await frame_queue.put(None)
     cap.release()
     print("[讀取器] 結束")
 
-async def inference():
-    print(f"[推理器] 啟動")
-    loop = asyncio.get_running_loop()
+async def inference(worker_id):
+    print(f"[推理器-{worker_id}] 啟動")
     while True:
-        frame = await frame_queue.get()
+        index, frame = await frame_queue.get()
         if frame is None:
             await result_queue.put(None)
-            print(f"[推理器] 收到結束訊號，離開。")
             break
         result = model.predict(frame, verbose=False)[0]
-        print(result.plot().shape)
+        print(f"[推理器-{worker_id}] {index} {result.plot().shape}")
 
 async def main():
-    tasks = [
-        asyncio.create_task(preprocess()),
-        asyncio.create_task(inference())
-    ]
+    tasks = [asyncio.create_task(preprocess())]
+    for i in range(args.num_workers):
+        tasks.append(asyncio.create_task(inference(i)))
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
