7d4ebb47acc8a340165d094c17ead744fc04dc0b
MarkovChenITRI <140595764+MarkovChenITRI@users.noreply.github.com>
2025-08-14 20:23:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import cv2, argparse
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+import queue
 
 parser = argparse.ArgumentParser(description="YOLO Async Inference with asyncio")
 parser.add_argument("--video_path", type=str, default="./data/video.mp4")
@@ -12,7 +13,7 @@ cap = cv2.VideoCapture(args.video_path)
 model = YOLO(args.tflite_model, task="detect")
 
 frame_queue = asyncio.Queue(maxsize=100)
-result_queue = asyncio.Queue(maxsize=100)
+result_queue = queue.Queue(maxsize=100)
 
 async def reader():
     print("[讀取器] 啟動")
@@ -29,7 +30,7 @@ async def reader():
 
 def predict(frame, model):
     result = model.predict(frame, verbose=False)[0]
-    result_queue.put((frame, model, result))
+    result_queue.put(result)
     
 async def predictor():
     loop = asyncio.get_running_loop()
@@ -40,7 +41,6 @@ async def predictor():
             frame = await frame_queue.get()
             if frame is None:
                 break
-            # 射後不理地送出任務
             fut = loop.run_in_executor(
                 executor,
                 predict_and_put,
@@ -49,16 +49,14 @@ async def predictor():
                 result_queue
             )
             futures.append(fut)
-        # 等所有任務完成
         await asyncio.gather(*futures)
-        await result_queue.put(None)
+        result_queue.put(None)
     print(f"[推理器] 結束")
 
 async def displayer():
     print("[顯示器] 啟動")
-    loop = asyncio.get_running_loop()
     while True:
-        result = await result_queue.get()
+        result = result_queue.get()
         if result is None:
             print(f"[顯示器] 收到結束訊號，離開。")
             break
