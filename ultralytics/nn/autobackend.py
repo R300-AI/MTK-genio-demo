93dbe25e3e202d949a1eda2c0e295bc9357e4ff2
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2025-08-15 02:12:17 +0000

    Update autobackend.py

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -490,7 +490,6 @@ class AutoBackend(nn.Module):
                 #     tflite_path=w, 
                 #     dla_path="<path_to_your_dla_model>",       
                 #     device= "<mdla3.0, mdla2.0 or vpu>"
-                #     admin_password='<enter_your_admin_password_here>'
                 # )
                 # LOGGER.info("Successfully loaded NeuronRT delegate for DLA inference")
 
